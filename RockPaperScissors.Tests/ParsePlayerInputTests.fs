2fb06b01ff9f0e8be328322728a4ac004843e9bc
Ondřej Švorc <ondrejsvorc@email.cz>
2024-06-16 11:24:54 +0200

    Split parse player input tests into multiple subtests

--- RockPaperScissors.Tests/ParsePlayerInputTests.fs
+++ RockPaperScissors.Tests/ParsePlayerInputTests.fs
@@ -4,7 +4,7 @@ open NUnit.Framework
 open Program
 
 [<TestFixture>]
-type ParsePlayerInputTests() =
+type ParsePlayerInputRockTests() =
 
     [<Test>]
     member _.``Parsing '1' should return Rock``() =
@@ -15,12 +15,44 @@ type ParsePlayerInputTests() =
         Assert.AreEqual(Choice Rock, parsePlayerInput "rock")
 
     [<Test>]
+    member _.``Parsing 'ROCK' should return Rock``() =
+        Assert.AreEqual(Choice Rock, parsePlayerInput "ROCK")
+
+    [<Test>]
+    member _.``Parsing 'Rock' should return Rock``() =
+        Assert.AreEqual(Choice Rock, parsePlayerInput "Rock")
+
+    [<Test>]
+    member _.``Parsing 'RoCk' should return Rock``() =
+        Assert.AreEqual(Choice Rock, parsePlayerInput "RoCk")
+
+
+[<TestFixture>]
+type ParsePlayerInputPaperTests() =
+
+    [<Test>]
     member _.``Parsing '2' should return Paper``() =
         Assert.AreEqual(Choice Paper, parsePlayerInput "2")
 
     [<Test>]
     member _.``Parsing 'paper' should return Paper``() =
         Assert.AreEqual(Choice Paper, parsePlayerInput "paper")
+
+    [<Test>]
+    member _.``Parsing 'PAPER' should return Paper``() =
+        Assert.AreEqual(Choice Paper, parsePlayerInput "PAPER")
+
+    [<Test>]
+    member _.``Parsing 'Paper' should return Paper``() =
+        Assert.AreEqual(Choice Paper, parsePlayerInput "Paper")
+
+    [<Test>]
+    member _.``Parsing 'PaPeR' should return Paper``() =
+        Assert.AreEqual(Choice Paper, parsePlayerInput "PaPeR")
+
+
+[<TestFixture>]
+type ParsePlayerInputScissorsTests() =
 
     [<Test>]
     member _.``Parsing '3' should return Scissors``() =
@@ -31,6 +63,22 @@ type ParsePlayerInputTests() =
         Assert.AreEqual(Choice Scissors, parsePlayerInput "scissors")
 
     [<Test>]
+    member _.``Parsing 'SCISSORS' should return Scissors``() =
+        Assert.AreEqual(Choice Scissors, parsePlayerInput "SCISSORS")
+
+    [<Test>]
+    member _.``Parsing 'Scissors' should return Scissors``() =
+        Assert.AreEqual(Choice Scissors, parsePlayerInput "Scissors")
+
+    [<Test>]
+    member _.``Parsing 'ScIsSoRs' should return Scissors``() =
+        Assert.AreEqual(Choice Scissors, parsePlayerInput "ScIsSoRs")
+
+
+[<TestFixture>]
+type ParsePlayerInputQuitTests() =
+
+    [<Test>]
     member _.``Parsing 'q' should return Quit``() =
         Assert.AreEqual(Quit, parsePlayerInput "q")
 
@@ -39,5 +87,37 @@ type ParsePlayerInputTests() =
         Assert.AreEqual(Quit, parsePlayerInput "quit")
 
     [<Test>]
-    member _.``Parsing invalid input should return Invalid``() =
+    member _.``Parsing 'QUIT' should return Quit``() =
+        Assert.AreEqual(Quit, parsePlayerInput "QUIT")
+
+    [<Test>]
+    member _.``Parsing 'Quit' should return Quit``() =
+        Assert.AreEqual(Quit, parsePlayerInput "Quit")
+
+    [<Test>]
+    member _.``Parsing 'QuIt' should return Quit``() =
+        Assert.AreEqual(Quit, parsePlayerInput "QuIt")
+
+
+[<TestFixture>]
+type ParsePlayerInputInvalidTests() =
+
+    [<Test>]
+    member _.``Parsing 'some invalid input' should return Invalid``() =
         Assert.AreEqual(Invalid, parsePlayerInput "some invalid input")
+
+    [<Test>]
+    member _.``Parsing empty input should return Invalid``() =
+        Assert.AreEqual(Invalid, parsePlayerInput "")
+
+    [<Test>]
+    member _.``Parsing whitespace input should return Invalid``() =
+        Assert.AreEqual(Invalid, parsePlayerInput "    ")
+
+    [<Test>]
+    member _.``Parsing numeric invalid input should return Invalid``() =
+        Assert.AreEqual(Invalid, parsePlayerInput "4")
+
+    [<Test>]
+    member _.``Parsing rock typo invalid input should return Invalid``() =
+        Assert.AreEqual(Invalid, parsePlayerInput "roc2k")
